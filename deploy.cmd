56eccdefd41e1b31825fc6e1706d860d63395a25
Juan Carlos López <jcarloslr10>
2018-05-28 18:36:27 +0200

    Added package rebuild for node-sass

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,8 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
  pushd "%DEPLOYMENT_TARGET%"
  call :ExecuteCmd !NPM_CMD! install
- IF !ERRORLEVEL! NEQ 0 goto error
+ call :ExecuteCmd !NPM_CMD! rebuild node-sass --force
+ :: IF !ERRORLEVEL! NEQ 0 goto error
  popd
 )
 
